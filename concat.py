efa916d5fc408fe402998de050227c52da604124
theCydonian <31706764+theCydonian@users.noreply.github.com>
2019-07-19 15:06:10 -0500

    concat.py no longer outputs row labels

--- concat.py
+++ concat.py
@@ -12,7 +12,7 @@ def main():
     for file in file_list:
         df_list.append(pd.read_csv(file)) # adds each csv to the datframe list
     concat_df = pd.concat(df_list) # concats the dataframes
-    concat_df.to_csv("concat_data.csv") # saves as new csv
+    concat_df.to_csv("concat_data.csv", index=False) # saves as new csv
 
 if __name__ == "__main__":
     main()
